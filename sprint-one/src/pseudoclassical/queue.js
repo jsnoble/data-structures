d3a49fe01d53d41466639528de8b34cb29110c34
Jared Noble <jsnoble1@gmail.com>
2014-12-11 12:10:44 -0700

    finished sprint one

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,28 @@
 var Queue = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.head = 0;
+  this.tail = 0;
+  this.list = {};
 };
 
+Queue.prototype.enqueue = function(value){
+  var key = this.tail;
+  this.list[key] = value;
+  this.tail++;
+};
 
+Queue.prototype.dequeue = function(){
+  if(this.head < this.tail){
+    var key = this.head;
+    results = this.list[key];
+    delete  this.list[key];
+    this.head++;
+    return results;
+  }
+
+};
+
+Queue.prototype.size = function(){
+  return this.tail - this.head;
+};
+
+var queue = new Queue();
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,27 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  this.top = 0;
+  this.list = {};
 };
 
+Stack.prototype.push = function(value){
+  var key = this.top;
+  this.list[key] = value;
+  this.top++;
+};
+
+Stack.prototype.pop = function(){
+  if (this.top) {
+    this.top--;
+    var key = this.top;
+    var result = this.list[key];
+    delete this.list[key];
+    return result;
+  }
+};
+
+Stack.prototype.size = function(){
+  return this.top;
+};
+
+var stack = new Stack();
 
