7409ca89e85577584ff1995f9e26f8c7df01f4da
Jared Noble <jsnoble1@gmail.com>
2014-12-12 12:49:10 -0700

    finished linked list

--- sprint-two/src/linkedList.js
+++ sprint-two/src/linkedList.js
@@ -4,12 +4,38 @@ var LinkedList = function(){
   list.tail = null;
 
   list.addToTail = function(value){
+    if (list.tail === null){
+      list.tail = Node(value);
+      list.head = list.tail;
+    } else {
+    var temp = Node(value);
+    list.tail.next = temp;
+    list.tail = temp;
+    }
   };
 
   list.removeHead = function(){
+    var prevNode = list.head.value;
+    var nextNode = list.head.next;
+    delete list.head;
+    list.head = nextNode;
+    return prevNode;
   };
 
   list.contains = function(target){
+    var thisNode = list.head;
+
+    while (thisNode.next){
+      if (thisNode.value === target){
+        return true;
+      } else {
+        thisNode = thisNode.next;
+      }
+    }
+    if(list.tail.value === target){
+      return true;
+    }
+    return false;
   };
 
   return list;
