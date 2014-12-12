2b5004ad65e0681dc9e375b0223974404b34855a
Jared Noble <jsnoble1@gmail.com>
2014-12-12 15:15:50 -0700

    second done

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,25 +1,37 @@
 var Tree = function(value){
   var newTree = {};
   newTree.value = value;
-
-  // your code here
-  newTree.children = null;  // fix me
-
+  _.extend(newTree, treeMethods);
+  newTree.children = [];
+// {children: [{value: 5},{value: 6}]  }
   return newTree;
+
 };
-
-
-
-
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value){
-
+  var node = {};
+  node.children = [];
+  _.extend(node, treeMethods);
+  node.value = value;
+  this.children.push(node);
 };
 
 treeMethods.contains = function(target){
+ for(var i = 0; i < this.children.length; i++){
+   if (this.children[i].value === target){
+     return true;
+   } if(this.children[i].children){
+     for (var j = 0; j< this.children[i].children.length; j++ ){
+       if(this.children[i].children[j].value === target){
+         return true;
+       }
+     }
+   }
+ }
 
+  return false;
 };
 
 
