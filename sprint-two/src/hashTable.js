fe53ae4ff3d08600e1b045ba10c849549f81be1d
Jared Noble <jsnoble1@gmail.com>
2014-12-12 22:49:36 -0700

    finished hash

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -5,14 +5,36 @@ var HashTable = function(){
 
 HashTable.prototype.insert = function(k, v){
   var i = getIndexBelowMaxForKey(k, this._limit);
+  var data = [k,v];
+
+  if(this._storage.get(i)){
+    this._storage.get(i).push(data);
+  } else {
+    this._storage.set (i , []);
+    this._storage.get(i).push(data);
+  }
 };
 
 HashTable.prototype.retrieve = function(k){
   var i = getIndexBelowMaxForKey(k, this._limit);
 
+ if(this._storage.get(i).length > 1){
+  console.log("yeah");
+   var results = this._storage.get(i);
+
+   for(var j = 0; j < results.length; j++) {
+     if(results[j][0] === k){
+       return results[j][1];
+     }
+    }
+ }
+  return this._storage.get(i)[0][1];
 };
 
 HashTable.prototype.remove = function(k){
+  var i = getIndexBelowMaxForKey(k, this._limit);
+    this._storage.set(i, []);
+  this._storage.get(i).push([null,null]);
 
 };
 
